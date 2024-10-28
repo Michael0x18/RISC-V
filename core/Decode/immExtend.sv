54df8c63021a740ab0385ee7f52f9d248ca9b116
Michael <michael2@valinor-2.local>
2024-10-27 22:25:44 -0500

    Fix up immediate decoding

--- core/Decode/immExtend.sv
+++ core/Decode/immExtend.sv
@@ -15,13 +15,13 @@ always_comb
         3'b001: immext = {{20{instr[31]}}, isntr[31:25], isntr[11:7]};
 
         // B-type
-        3'b010: immext = {{20{instr[31]}}, instr[7], instr[31:25], instr[11:8], 1'b0};
+        3'b010: immext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
 
         // J-type
         3'b011: immext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
 
         // U-type
-        3'b100: immext = {instr[31:12]};
+        3'b100: immext = {instr[31:12], 12'b0};
 
         default: immext = 32'bx; // undefined
     endcase
